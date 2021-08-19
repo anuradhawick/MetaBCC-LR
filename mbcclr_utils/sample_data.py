99b3fa3ec86f7633ec07e5bbf362c0e7333aafc5
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2021-08-19 15:53:38 +1000

    Update sample_data.py
    Path bug fix.
    
    Previously absolute paths were broken due to `./` prefix.

--- mbcclr_utils/sample_data.py
+++ mbcclr_utils/sample_data.py
@@ -5,8 +5,8 @@ import logging
 logger = logging.getLogger('MetaBCC-LR')
 
 def sample(output, sample_count, ground_truth):
-    p3_data = np.loadtxt(f"./{output}/profiles/3mers", dtype=float)
-    p15_data = np.loadtxt(f"./{output}/profiles/15mers", dtype=float)
+    p3_data = np.loadtxt(f"{output}/profiles/3mers", dtype=float)
+    p15_data = np.loadtxt(f"{output}/profiles/15mers", dtype=float)
 
     logger.debug(f"3mer data shape {str(p3_data.shape)}")
     logger.debug(f"15mer data shape {str(p15_data.shape)}")
@@ -25,9 +25,9 @@ def sample(output, sample_count, ground_truth):
     p3_sampled = p3_data[idx_chosen]
     p15_sampled = p15_data[idx_chosen]
 
-    np.save(f"./{output}/profiles/3mers_sampled.npy", p3_sampled)
-    np.save(f"./{output}/profiles/15mers_sampled.npy", p15_sampled)
+    np.save(f"{output}/profiles/3mers_sampled.npy", p3_sampled)
+    np.save(f"{output}/profiles/15mers_sampled.npy", p15_sampled)
 
     if ground_truth is not None:
         ground_truth_sampled = ground_truth[idx_chosen]
-        np.save(f"./{output}/misc/filtered_truth_sampled.npy", ground_truth_sampled)
+        np.save(f"{output}/misc/filtered_truth_sampled.npy", ground_truth_sampled)
